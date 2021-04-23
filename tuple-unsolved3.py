899d5738d391665a41dc7af1d4c72859bd123f03
shahin217 <75066626+shahin217@users.noreply.github.com>
2021-04-23 16:33:55 +0530

    Added tuple-unsolved ques3

---
+++ tuple-unsolved3.py
@@ -0,0 +1,9 @@
+def Convert(tup, di):
+    di = dict(tup)
+    return di
+      
+# Driver Code 
+tups = [("a", 1), ("b", 2), ("c", 3), 
+    ("d", 4), ("e", 5), ("f", 6)]
+dictionary = {}
+print (Convert(tups, dictionary))
