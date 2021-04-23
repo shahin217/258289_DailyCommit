45f6e424def4eaf15f23268a996fd9235d39b75d
shahin217 <75066626+shahin217@users.noreply.github.com>
2021-04-23 11:46:29 +0530

    unsolved ques added

---
+++ if-elif-else-unsolved2.py
@@ -0,0 +1,13 @@
+user_input = input("Enter anything from: lol, rofl, lmk, smh\n")
+
+if user_input == "lol":
+    print("laughing out loud")
+elif user_input == "rofl":
+    print("rolling on the floor laughing")
+elif user_input == "lmk":
+    print("let me know")
+elif user_input == "smh":
+    print("shaking my head")
+else:
+    print("Invalid input\ntry with another one")
+
